279a64715b1efeb24f1daa81903bce81cba7bca1
moluopro <moluopro@qq.com>
2026-06-28 15:50:11 +0800

    Remove sample calculator API

--- lib/abutil.dart
+++ lib/abutil.dart
@@ -3,9 +3,3 @@ library abutil;
 export 'src/platform.dart';
 export 'src/reboot.dart';
 export 'src/update_check.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
